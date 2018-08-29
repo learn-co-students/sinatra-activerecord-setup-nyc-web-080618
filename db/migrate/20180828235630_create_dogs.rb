c31055f6b3a9d37d514a92d1f9baebc2ae4877c2
github username <ariellenramirez@gmail.com>
2018-08-28 20:03:01 -0400

    take 2

--- db/migrate/20180828235630_create_dogs.rb
+++ db/migrate/20180828235630_create_dogs.rb
@@ -1,8 +1,10 @@
 class CreateDogs < ActiveRecord::Migration
+  #using change elminates multiple steps
  def change
    create_table :dogs do |t|
      t.string :name
      t.string :breed
+
   end
  end
 end
